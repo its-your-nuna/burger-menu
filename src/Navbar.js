d2480a637d5a681673206637243b8182904a1c0b
its-your-nuna <200103140@stu.sdu.edu.kz>
2022-08-18 01:54:57 +0600

    add

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css'
 import { Nav, Navbar} from "react-bootstrap";
-// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Navigation() {
   return (
